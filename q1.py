e2a2ac58f984e7276d0393b95ab7557c5047e103
umerhasan17 <umerhasan17@gmail.com>
2018-10-17 16:47:24 +0100

    Inefficient solution to q1

--- q1.py
+++ q1.py
@@ -6,4 +6,13 @@ import numpy as np
 def question01(portfolios):
   # modify and then return the variable below
   answer = 0
+  for i in range(len(portfolios)):
+    for j in range(len(portfolios)):
+      # exclusive or
+      answer = max(answer, portfolios[i] ^ portfolios[j])
+  
+  print (answer)
   return answer
+
+question01([15, 8, 6, 7])
+question01([9, 7, 12, 2])
